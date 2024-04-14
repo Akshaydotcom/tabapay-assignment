fb48ef07d73a116192e0c577940f2a08b2770acc
AkshayGB <akshaybhandaryg1@gmail.com>
2024-04-14 12:59:26 -0700

    Created a node.js and express project to create basic APIs to send JSON data. And restructured the repository appropriately

---
+++ frontend/README.md
@@ -0,0 +1,25 @@
+# Tabapay-UI Project Part-5 #
+
+## Description ##
+Add-on from Part 4, Created and Added JSON Data to simulate categories of textual data.
+
+
+## Installation ##
+Since this project is built using Vite, please run:
+1. npm install
+2. cd ./tabapay-ui/
+3. npm run dev
+
+Open Browser with the url given by last command.
+
+To check production, run "npm run build"
+
+### Note ###
+1. Also, if the need arises, we could use pagination on top of the existing responsive page in case of more categories, and/or more textual information. 
+   This could be done by using an API to get data from the server, thus allowing us to limit the number/size of the response.
+
+2. Depending on the relation between the categories of text, we could group them first, and then show different categories in the each group. 
+   We could try different Tabs for different Groups or Use a similar Tree View with the group headings as the first level of nodes, so to speak.
+
+### Update ###
+To check Live Version of the project: [Website](https://tabapay-ui-agigabyte.netlify.app)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,105 @@
+import { useState, useEffect } from "react";
+import { createPortal } from "react-dom";
+import "./App.css";
+import logo from "../public/p2.png";
+import { navData } from "./assets/navData";
+import TreeNode from "./components/TreeNode";
+import PageContent from "./components/PageContent";
+import ModalContainer from "./helperComponents/ModalContainer";
+function App() {
+  const [openNodeIdsByLevel, setOpenNodeIdsByLevel] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [mobile, setMobile] = useState(window.innerWidth <= 500);
+  const [isSidePanelOpen, setIsSidePanelOpen] = useState(false);
+  //lifted state up from TreeNode component to keep track of Open Nodes by Levels in the Tree
+  const toggleNode = (nodeId, level) => {
+    setOpenNodeIdsByLevel((prevState) => ({
+      ...prevState,
+      [level]: prevState[level] === nodeId ? null : nodeId,
+    }));
+  };
+
+  /*function to check if the correct node is open using the state 
+  variable to check which node was open previously*/
+  const isNodeOpen = (nodeId, level) => {
+    return openNodeIdsByLevel[level] === nodeId;
+  };
+
+  //function to set the message/title of the modal pop up
+  function showTitle(message) {
+    setMessage(message);
+    setIsModalOpen(true);
+  }
+
+  //function to check inner width to check if device type is mobile
+  function handleWindowSizeChange() {
+    setMobile(window.innerWidth <= 500);
+  }
+
+  //Added useEffect to add and remove eventlistener on resizing to check the device type
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  /*
+  render TreeNode if navData is present, added modal pop-up using createPortal, added newly created
+  component to show the title/label of the node clicked by the user. 
+  Added header and footers, changed html semantics 
+  */
+  return (
+    <>
+      <header>
+        <img src={logo} alt="logo" className="logo" />
+        <h1>Developers UI Project</h1>
+      </header>
+      <div className="main-content">
+        <aside>
+          {navData && (
+            <TreeNode
+              node={navData}
+              level={0}
+              showTitle={showTitle}
+              toggleNode={toggleNode}
+              isNodeOpen={isNodeOpen}
+            />
+          )}
+        </aside>
+        <main>
+          <article>
+            {!mobile && <PageContent title={message} />}
+            {mobile && isSidePanelOpen && (
+              <PageContent
+                title={message}
+                isSidePanelOpen={isSidePanelOpen}
+                setIsSidePanelOpen={setIsSidePanelOpen}
+              />
+            )}
+          </article>
+        </main>
+      </div>
+
+      <footer>
+        <img src={logo} alt="logo" className="logo" />
+        <pre>@ 2023-2024 Personal Made with React.js</pre>
+      </footer>
+      {isModalOpen &&
+        message.length > 0 &&
+        createPortal(
+          <ModalContainer
+            message={message}
+            onClose={() => {
+              setIsModalOpen(false);
+              setIsSidePanelOpen(true);
+            }}
+          />,
+          document.body
+        )}
+    </>
+  );
+}
+
+export default App;
