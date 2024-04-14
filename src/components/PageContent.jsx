32fcfe2b7fc981838f77823ff5fdc262ca2b7897
AkshayGB <akshaybhandaryg1@gmail.com>
2024-04-14 11:25:27 -0700

    Created JSON object to simulate data as depicted in the question. Used this data in PageContent component.

--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -1,26 +1,39 @@
+import { pageData } from "../assets/pageData.js";
+
 export default function PageContent({
-    title,
-    isSidePanelOpen,
-    setIsSidePanelOpen,
-  }) {
-  
-    return (
-      <div className={`panel ${isSidePanelOpen ? "open" : ""}`}>
-        {isSidePanelOpen ? (
-          <div className="sidePanel-header">
-            <h1>{title}</h1>
-            <button
-              onClick={() => {
-                setIsSidePanelOpen(false);
-              }}
-            >
-              Close
-            </button>
-          </div>
-        ) : (
+  title,
+  isSidePanelOpen,
+  setIsSidePanelOpen,
+}) {
+  return (
+    <div className={`panel ${isSidePanelOpen ? "open" : ""}`}>
+      {isSidePanelOpen ? (
+        <div className="sidePanel-header">
           <h1>{title}</h1>
-        )}
+          <button
+            onClick={() => {
+              setIsSidePanelOpen(false);
+            }}
+          >
+            Close
+          </button>
+        </div>
+      ) : (
+        <h1>{title}</h1>
+      )}
+      <div className="different-category-content">
+        {pageData?.length > 0 &&
+          pageData.map((object, index) => {
+            return (
+              <div key={index}>
+                <details>
+                  <summary>{object.title}</summary>
+                  <span>{object.content}</span>
+                </details>
+              </div>
+            );
+          })}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
